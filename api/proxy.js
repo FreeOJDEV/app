2568dbaa9fe64b4f06780a3af25a35f337eeaae5
FreeOJDEV <FojPro@outlook.com>
2024-10-19 02:09:43 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://android11react.osrc.com";//your website url
+  let target = "https://android.blueedge.me/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
