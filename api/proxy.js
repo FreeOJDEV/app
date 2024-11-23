8b4f50c504aa35944c06408a2c33a89b0ab38704
FreeOJDEV <FojPro@outlook.com>
2024-11-23 15:36:10 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://android.blueedge.me/";//your website url
+  let target = "https://html-classic.itch.zone/html/7550065/itch/index.html";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
